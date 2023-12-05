3feedf505b84a02c7ee0de0cd5203afd065ca206
Artem <artem@yugaev.ru>
2023-12-05 14:39:41 +0300

    лол

--- Markbook/Markbook/urls.py
+++ Markbook/Markbook/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mainapp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.main_page, name='Main')
 ]
