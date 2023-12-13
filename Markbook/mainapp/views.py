baa26524cc9f198ec4716a4d7182ce388a6fdfa5
F1ower <Daniil.shirkin005@gmail.com>
2023-12-13 23:24:30 +0300

    something

--- Markbook/mainapp/views.py
+++ Markbook/mainapp/views.py
@@ -10,19 +10,21 @@ def main_page(request):
     context = {}
     form = StudentLoginForm
     model = Student
-    if request.method == "POST":
-
-        if request.session.test_cookie_worked():
-            field = form(request.POST)
-            if field.is_valid():
-                user = Student.objects.get(code = field.data['code'])
-                request.session["student_id"] = user.id
-                return redirect('/tables')
+    if(request.session["student_id"]!=None):
+        if request.method == "POST":
+            if request.session.test_cookie_worked():
+                field = form(request.POST)
+                if field.is_valid():
+                    user = Student.objects.get(code = field.data['code'])
+                    request.session["student_id"] = user.id
+                    return redirect('/tables')
+        else:
+            field = form()
+            context['field'] = field
+        request.session.set_test_cookie()
+        return render(request, 'mainpage.html',context)
     else:
-        field = form()
-        context['field'] = field
-    request.session.set_test_cookie()
-    return render(request, 'mainpage.html',context)
+        return redirect("Main")
 
 def tables(request):
     user = Student.objects.get(id = request.session['student_id'])
